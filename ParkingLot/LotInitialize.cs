015fcf8bd953d1f3175bbe5fb214128aa6fb838b
Jiankun YANG <JYang53@slb.com>
2020-11-21 18:54:59 +0800

    feat: parking boy can given correct error messeage when provided with used ticket

---
+++ ParkingLot/LotInitialize.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot
+{
+    public class LotInitialize
+    {
+        public static Dictionary<string, Car> FillLotWithSomeCars()
+        {
+            var cars = new List<Car>()
+            {
+                new Car("RJ_12784"),
+                new Car("RJ_34543"),
+                new Car("RJ_12446"),
+            };
+            var parkingLot = new Dictionary<string, Car>();
+
+            foreach (var car in cars)
+            {
+                parkingLot.Add(car.PlateNumber, car);
+            }
+
+            return parkingLot;
+        }
+
+        public static Dictionary<string, Car> FillLotWithTenCars()
+        {
+            var cars = new List<Car>()
+            {
+                new Car("RJ_12784"),
+                new Car("RJ_34543"),
+                new Car("RJ_12446"),
+                new Car("RJ_15456"),
+                new Car("RJ_45731"),
+                new Car("RJ_45372"),
+                new Car("RJ_45326"),
+                new Car("RJ_78341"),
+                new Car("RJ_45387"),
+                new Car("RJ_78635"),
+            };
+            var parkingLot = new Dictionary<string, Car>();
+
+            foreach (var car in cars)
+            {
+                parkingLot.Add(car.PlateNumber, car);
+            }
+
+            return parkingLot;
+        }
+    }
+}
---
+++ ParkingLotTest/TestStory2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ParkingLot;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class TestStory2
+    {
+        [Fact]
+        public void Should_ParkingBoy_Return_Correct_Error_Message_Given_Wrong_Tickect()
+        {
+            //Given
+            var boy = new ParkingBoy();
+            Dictionary<string, Car> parkingLot = LotInitialize.FillLotWithSomeCars();
+            var ticket = "SuperPark: 456213154";
+            Car expectedCar = null;
+            string expectedMessage = "Unrecognized parking ticket.";
+            string actualMessage;
+
+            //When
+            var actualCar = boy.Fetch(ticket, parkingLot, out actualMessage);
+
+            //Then
+            Assert.Equal(expectedCar, actualCar);
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+
+        [Fact]
+        public void Should_ParkingBoy_Return_Correct_Error_Message_Given_Used_Tickect()
+        {
+            //Given
+            var boy = new ParkingBoy();
+            Dictionary<string, Car> parkingLot = LotInitialize.FillLotWithSomeCars();
+            var ticket = "SuperPark: RJ_12784";
+            boy.Fetch(ticket, parkingLot);
+            Car expectedCar = null;
+            string expectedMessage = "Unrecognized parking ticket.";
+            string actualMessage;
+
+            //When
+            var actualCar = boy.Fetch(ticket, parkingLot, out actualMessage);
+
+            //Then
+            Assert.Equal(expectedCar, actualCar);
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+    }
+}
