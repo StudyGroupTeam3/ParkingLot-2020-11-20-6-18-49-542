d090811e59b41dc6623d589d23ea3558b3089750
Ying Fu <YFu3@slb.com>
2020-11-21 22:29:06 +0800

    feat: a first test for story1 and story2

--- ParkingLot/ParkingBoy.cs
+++ ParkingLot/ParkingBoy.cs
@@ -7,29 +7,51 @@ namespace ParkingLot
 {
     public class ParkingBoy
     {
-        private string car;
-        private string ticket;
         private List<string> tickets = new List<string>()
         {
             "ticket-1-1", "ticket-1-2", "ticket-1-3", "ticket-1-4", "ticket-1-5",
             "ticket-1-6", "ticket-1-7", "ticket-1-8", "ticket-1-9", "ticket-1-10",
         };
 
+        private List<string> ticketsUsed = new List<string>();
+
+        private Dictionary<string, string> carAndTicketInformation = new Dictionary<string, string>();
+
+        public bool HasPosition()
+        {
+            return carAndTicketInformation.Keys.Count < 10;
+        }
+
         public string ParkingCar(string car)
         {
-            this.car = car;
-            return tickets[0];
+            if (HasPosition())
+            {
+                string ticket = tickets[0];
+                carAndTicketInformation.Add(ticket, car);
+                tickets.Remove(ticket);
+                return ticket;
+            }
+
+            return "Not enough position.";
         }
 
         public string FetchCar(string ticket)
         {
-            string carTicket = ParkingCar(car);
-            if (ticket != carTicket || ticket == null)
+            if (string.IsNullOrEmpty(ticket))
             {
-                return "No car fetched";
+                return "Please provide your parking ticket.";
             }
 
-            return car;
+            if (!carAndTicketInformation.ContainsKey(ticket))
+            {
+                return "Unrecognized parking ticket.";
+            }
+
+            string fecthedCar = carAndTicketInformation[ticket];
+            ticketsUsed.Add(ticket);
+            carAndTicketInformation.Remove(ticket);
+            //tickets.Add(ticket);
+            return fecthedCar;
         }
     }
 }
--- ParkingLotTest/UnitTest1.cs
+++ ParkingLotTest/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace ParkingLotTest
 {
     using Moq;
@@ -18,15 +21,73 @@ namespace ParkingLotTest
             string actual = parkingBoy.ParkingCar(car);
 
             // then
-            Assert.Equal("ticket-1-1", actual);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
         [InlineData("ticket-1-1")]
-        public void ShouldReturnACarGivenATicket(string ticket)
+        public void ShouldReturnCarAGivenTicketOfCarA(string ticket)
         {
             // given
             string expected = "carA";
+
+            // when
+            ParkingBoy parkingBoy = new ParkingBoy();
+            string carA = "carA";
+            string carB = "carB";
+            parkingBoy.ParkingCar(carA);
+            parkingBoy.ParkingCar(carB);
+            string actual = parkingBoy.FetchCar(ticket);
+
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("ticket-1-2")]
+        public void ShouldReturnCarBGivenTicketOfCarB(string ticket)
+        {
+            // given
+            string expected = "carB";
+
+            // when
+            ParkingBoy parkingBoy = new ParkingBoy();
+            string carA = "carA";
+            string carB = "carB";
+            parkingBoy.ParkingCar(carA);
+            parkingBoy.ParkingCar(carB);
+            string actual = parkingBoy.FetchCar(ticket);
+
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("ticket-1-1")]
+        public void ShouldReturnNoFetchedCarGivenUsedTicket(string ticket)
+        {
+            // given
+            string expected = "Unrecognized parking ticket.";
+
+            // when
+            ParkingBoy parkingBoy = new ParkingBoy();
+            string carA = "carA";
+            string carB = "carB";
+            parkingBoy.ParkingCar(carA);
+            parkingBoy.ParkingCar(carB);
+            parkingBoy.FetchCar(ticket);
+            string actual = parkingBoy.FetchCar(ticket);
+
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("")]
+        public void ShouldHaveNoFetchedGivenNullTicket(string ticket)
+        {
+            // given
+            string expected = "Please provide your parking ticket.";
 
             // when
             ParkingBoy parkingBoy = new ParkingBoy();
@@ -34,7 +95,48 @@ namespace ParkingLotTest
             string actual = parkingBoy.FetchCar(ticket);
 
             // then
-            Assert.Equal("carA", actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("ticket-1-2")]
+        public void ShouldHaveNoFetchedGivenWrongTicket(string ticket)
+        {
+            // given
+            string expected = "Unrecognized parking ticket.";
+
+            // when
+            ParkingBoy parkingBoy = new ParkingBoy();
+            parkingBoy.ParkingCar(expected);
+            string actual = parkingBoy.FetchCar(ticket);
+
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("car-11")]
+        [InlineData("car-1")]
+        [InlineData("")]
+        public void ShouldHaveNoTicketWhenPositionIsFull(string car)
+        {
+            // given
+            string expected = "Not enough position.";
+            // when
+            List<string> cars = new List<string>()
+            {
+                "car-1", "car-2", "car-3", "car-4", "car-5",
+                "car-6", "car-7", "car-8", "car-9", "car-10",
+            };
+            ParkingBoy parkingBoy = new ParkingBoy();
+            for (int i = 0; i < cars.Count; i++)
+            {
+                parkingBoy.ParkingCar(cars[i]);
+            }
+
+            string actual = parkingBoy.ParkingCar(car);
+            // then
+            Assert.Equal(expected, actual);
         }
     }
 }
