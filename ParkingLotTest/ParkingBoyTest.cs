6fec47719d9bdee4a20c4b9e07fd83dc72d32001
Ziyuan <zxu25@slb.com>
2020-11-22 00:29:04 +0800

    feat: create ParkingBoy to park cars

---
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ParkingLot;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingBoyTest
+    {
+        [Fact]
+        public void Should_generate_ParkingBoy()
+        {
+            // given
+            var parkingBoy = new ParkingBoy();
+            // then
+            Assert.NotNull(parkingBoy);
+        }
+    }
+}
