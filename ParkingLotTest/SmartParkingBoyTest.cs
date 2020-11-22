c2b9cfe388ac490320653fd891b819d735deceef
Ziyuan <zxu25@slb.com>
2020-11-22 11:52:02 +0800

    feat: US4 AC1 AC2 add files for last commit

---
+++ ParkingLotTest/SmartParkingBoyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLotTest
+{
+    using Xunit;
+    using ParkingLot;
+
+    public class SmartParkingBoyTest
+    {
+        [Fact]
+        public void Should_always_park_cars_to_the_parking_lot_which_contains_more_empty_positions()
+        {
+            // given
+            List<ParkingLot> parkingLotList = new List<ParkingLot> { new ParkingLot(0, 1), new ParkingLot(1, 2) };
+            var smartParkingBoy = new SmartParkingBoy(parkingLotList);
+            var expectedCar = new Car();
+            // when
+            var parkingTicket = smartParkingBoy.ParkCar(expectedCar, out string errorMessagePark1);
+            // then
+            Assert.True(parkingTicket.ParkingLotId == 1);
+        }
+    }
+}
---
+++ ParkingLotTest/SuperSmartParkingBoyTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLotTest
+{
+    using Xunit;
+
+    public class SuperSmartParkingBoyTest
+    {
+    }
+}
