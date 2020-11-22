7a0137583e7042b791db674e5ff046679b085e96
Jiankun YANG <JYang53@slb.com>
2020-11-22 16:22:46 +0800

    feat: manager can send parking boy to park

--- ParkingLotTest/Story6AC1.cs
+++ ParkingLotTest/Story6AC1.cs
@@ -22,5 +22,33 @@ namespace ParkingLotTest
             //Then
             Assert.Equal(true, result);
         }
+
+        [Theory]
+        [InlineData("RJ_36528", "SuperPark: RJ_36528")]
+        [InlineData("BJ_88888", "SuperPark: BJ_88888")]
+        [InlineData("NY_34987713", "SuperPark: NY_34987713")]
+        public void Should_Manager_Can_Tell_Boy_To_Park(string plateNumber, string parkingTicket)
+        {
+            //Given
+            var manager = new ParkingManager("Chris");
+            var employee = new ParkingBoy("Jack");
+            manager.HireBoy(employee);
+            string parkMessage = string.Empty;
+            var car = new Car(plateNumber);
+            var parkingLots = new List<CarLot<string, Car>>()
+            {
+                LotInitialize.FillLotWithCars(10, 20),
+                LotInitialize.FillLotWithCars(8, 30),
+            };
+
+            string expected = parkingTicket;
+
+            //When
+            var result = manager.ManagerPark(car, parkingLots, out parkMessage);
+
+            //Then
+            Assert.IsType<string>(result);
+            Assert.Equal(expected, result);
+        }
     }
 }
