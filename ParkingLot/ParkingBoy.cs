001b23044a312ebd488f7f9c88ca360ae64d355c
Ying Fu <YFu3@slb.com>
2020-11-22 16:23:14 +0800

    test: Only for story1,2,3

--- ParkingLot/ParkingBoy.cs
+++ ParkingLot/ParkingBoy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Dynamic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -10,28 +11,44 @@ namespace ParkingLot
     public class ParkingBoy
     {
         private int capacity = 10;
+        private int maxParkingLots = 2;
         private Dictionary<string, string> carAndTicketInformation = new Dictionary<string, string>();
 
         public string ParkingCar(string car)
         {
-            if (carAndTicketInformation.Count < capacity)
+            for (int i = 0; i < maxParkingLots; i++)
             {
-                ParkingLots parkingLot = new ParkingLots("1");
-                List<string> tickets = parkingLot.GetTickets();
+                if (carAndTicketInformation.Count >= i * capacity && carAndTicketInformation.Count < (i + 1) * capacity)
+                {
+                    ParkingLots parkingLot = new ParkingLots($"{i + 1}");
+                    List<string> tickets = parkingLot.GetTickets();
+                    string ticket = tickets[carAndTicketInformation.Count - i * capacity];
+                    carAndTicketInformation.Add(ticket, car);
+                    return ticket;
+                }
+            }
+
+            /*
+            if (carAndTicketInformation.Count < maxParkingLots * capacity)
+            {
+                List<ParkingLots> parkingLotses = new List<ParkingLots>();
+                List<string> tickets = new List<string>();
+                for (int i = 0; i < maxParkingLots; i++)
+                {
+                    ParkingLots parkingLot = new ParkingLots($"{i} + 1");
+                    for (int j = 0; j < capacity; j++)
+                    {
+                        tickets.Add(parkingLot.GetTickets()[j]);
+                    }
+
+                    parkingLotses.Add(parkingLot);
+                }
+
                 string ticket = tickets[carAndTicketInformation.Count];
                 carAndTicketInformation.Add(ticket, car);
                 return ticket;
             }
-
-            if (carAndTicketInformation.Count >= capacity && carAndTicketInformation.Count < 2 * capacity)
-            {
-                ParkingLots parkingLot = new ParkingLots("2");
-                List<string> tickets = parkingLot.GetTickets();
-                string ticket = tickets[carAndTicketInformation.Count - capacity];
-                carAndTicketInformation.Add(ticket, car);
-                return ticket;
-            }
-
+            */
             return "Not enough position.";
         }
 
@@ -49,7 +66,6 @@ namespace ParkingLot
 
             string fecthedCar = carAndTicketInformation[ticket];
             carAndTicketInformation.Remove(ticket);
-            //tickets.Add(ticket);
             return fecthedCar;
         }
     }
